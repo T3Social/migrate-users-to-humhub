26ce13f20783a25a2be21f2aa1d2a3b31a26adfb
Sony AK <sony@sony-ak.com>
2014-11-05 15:05:40 +0700

    Create include_db_config.php

---
+++ include_db_config.php
@@ -0,0 +1,11 @@
+<?php
+	// this is the setting for database of that contains user table that you want to migrate to HumHub
+	$dbHost = "database_host";
+  $dbUsername = "database_username";
+  $dbPassword = "database_password";
+  $dbName = "database_name";
+  
+  $dbConn = mysql_connect($dbHost, $dbUsername, $dbPassword) or die("Database now under site maintenance! Please be patient and we will back soon!");
+  mysql_select_db($dbName, $dbConn) or die("Database name is not available !!");
+	mysql_set_charset('utf8', $dbConn);
+  mysql_query("SET NAMES 'utf8'", $dbConn);
